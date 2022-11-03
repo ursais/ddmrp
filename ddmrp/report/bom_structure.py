10d72404032332d9be0c0335d29b8409ff4b6d91
Balaji Kannan <bkannan@ursainfosystems.com>
2022-11-03 09:30:57 -0700

    [FIX] base class name corrected.

--- ddmrp/report/bom_structure.py
+++ ddmrp/report/bom_structure.py
@@ -6,7 +6,7 @@ from odoo import api, models
 
 
 class ReportBomStructure(models.AbstractModel):
-    _inherit = "report.mrp.bom_structure_report"
+    _inherit = "report.mrp.report_bom_structure"
 
     @api.model
     def _get_child_vals(self, record, level, qty, uom):
